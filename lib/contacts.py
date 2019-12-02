2dd1b27da86836b5a37a164df55676b6c2a52e7c
Tre <amack4693@gmail.com>
2019-12-01 22:38:29 -0500

    added to contact class

--- lib/contacts.py
+++ lib/contacts.py
@@ -1,6 +1,15 @@
 print("Welcome to your contact list.")  
 
+UI = """
+[1] Add Contact
+[2]View Contacts
+[3]Search Contacts
+[4]Exit
+"""
+
 class Contact:
-    def __init__(self, name):
+    def __init__(self, name=None, phone=None, email=None, address=None):
         self.name = name
-        
+        self.phone = phone
+        self.email = email
+        self.address = address
